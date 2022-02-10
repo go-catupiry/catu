9d1cca1636a183972841658717ec6b879bdac810
albertosouza <alberto.souza.99@gmail.com>
2022-02-09 22:59:00 -0300

    Update base model to add some json api tags

--- models/BaseModel.go
+++ models/BaseModel.go
@@ -6,8 +6,8 @@ import (
 
 type Base struct {
 	ID        uint64    `gorm:"column:id;primary_key"  json:"id"`
-	CreatedAt time.Time `gorm:"column:createdAt;type:datetime;not null" json:"createdAt"`
-	UpdatedAt time.Time `gorm:"column:updatedAt;type:datetime;not null" json:"updatedAt"`
+	CreatedAt time.Time `gorm:"column:createdAt;type:datetime;not null" json:"createdAt" jsonapi:"attr,updatedAt,iso8601"`
+	UpdatedAt time.Time `gorm:"column:updatedAt;type:datetime;not null" json:"updatedAt" jsonapi:"attr,updatedAt,iso8601"`
 
 	LinkPermanent string `gorm:"-" json:"linkPermanent"`
 }
