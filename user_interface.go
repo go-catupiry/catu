0ef97d13da51565a5ffd6e5150c28c10772a3692
albertosouza <alberto.souza.99@gmail.com>
2022-02-11 12:34:18 -0300

    Add some usefull fields in user interface

--- user_interface.go
+++ user_interface.go
@@ -1,5 +1,27 @@
 package catu
 
 type UserInterface interface {
+	// getters:
+	GetID() string
+	SetID(id string) error
 	GetRoles() []string
+	SetRoles(v []string) error
+	AddRole(role string) error
+	RemoveRole(role string) error
+	GetEmail() string
+	SetEmail(v string) error
+	GetUsername() string
+	SetUsername(v string) error
+	GetDisplayName() string
+	SetDisplayName(v string) error
+	GetFullName() string
+	SetFullName(v string) error
+	GetLanguage() string
+	SetLanguage(v string) error
+	IsActive() bool
+	SetActive(blocked bool) error
+	IsBlocked() bool
+	SetBlocked(blocked bool) error
+	// FillById
+	FillById(ID string) error
 }
