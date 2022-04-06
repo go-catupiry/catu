7388cc084e20d444f0d49fa2fc82e76079e995f7
albertosouza <alberto.souza.99@gmail.com>
2022-04-06 20:42:46 -0300

    Remove uneed comments

--- Pluginer.go
+++ Pluginer.go
@@ -3,16 +3,4 @@ package catu
 type Pluginer interface {
 	Init(app *App) error
 	GetName() string
-
-	// BeforeBindMiddlewares(app *App) error
-	// BindMiddlewares(app *App) error
-	// AfterBindMiddlewares(app *App) error
-
-	// BeforeBindRoutes(app *App) error
-	// BindRoutes(app *App) error
-	// AfterBindRoutes(app *App) error
-
-	// SetTemplateFuncMap(app *App) error
-
-	// OnBootstrap(app *App) error
 }
